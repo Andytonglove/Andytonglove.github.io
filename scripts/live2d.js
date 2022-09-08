d539c33975dafd9473747b069c95305847e15e18
ParatrooperAndy <1468492471@qq.com>
2022-09-08 18:28:03 +0800

    backup live2d

--- scripts/live2d.js
+++ scripts/live2d.js
@@ -1,3 +1,5 @@
+// Hexo 注入代码
+hexo.extend.injector.register('head_begin', '<link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/css/all.min.css">', 'default');
 // Fluid 注入代码
 hexo.extend.filter.register('theme_inject', function(injects) {
   injects.head.raw('default', '<script src="https://andytonglove.github.io/live2d-widget/autoload.js"></script>');
